7c190fdb3be7fcf32c3e630894dea812e97ef1f0
Femi <oluwafemi.za@gmail.com>
2023-12-27 14:28:08 +0000

    Updates

---
+++ component---src-pages-privacy-js-b66c3c746fa887156762.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_theme_portfolio_minimal=self.webpackChunkgatsby_theme_portfolio_minimal||[]).push([[844],{8897:function(e,t,n){n.r(t),n.d(t,{default:function(){return a}});var l=n(7294),i=n(9159);function a(){return l.createElement(l.Fragment,null,l.createElement(i.pQ,{title:"Privacy Policy",useTitleTemplate:!0,noIndex:!0}),l.createElement(i.T3,null,l.createElement(i.Ek,{sectionId:"privacy",heading:"Privacy Policy"})))}}}]);
+//# sourceMappingURL=component---src-pages-privacy-js-b66c3c746fa887156762.js.map
